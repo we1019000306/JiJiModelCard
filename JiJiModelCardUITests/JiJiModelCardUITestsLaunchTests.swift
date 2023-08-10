d7cab98ce293ddcb824825edbeef55db64ef8505
Jackie Liu <we1019000306@163.com>
2023-08-10 20:48:05 +0800

    Initial Commit

---
+++ JiJiModelCardUITests/JiJiModelCardUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  JiJiModelCardUITestsLaunchTests.swift
+//  JiJiModelCardUITests
+//
+//  Created by 刘明远 on 2023/8/10.
+//
+
+import XCTest
+
+final class JiJiModelCardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
